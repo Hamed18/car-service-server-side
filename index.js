a4e80b02b56e4977396fa32074b7eebbd6148f08
hamed18 <hamedcnec@gmail.com>
2024-08-28 00:59:42 +0600

    Added single service load api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 4000;
@@ -30,11 +30,26 @@ async function run() {
 
 	const serviceCollection = client.db('carDoctor').collection('services');
 
+	// client: service section (find multiple document of the collection)
     app.get('/services', async(req, res) =>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+
+	// added single service load API (find a specific document of the collection)
+	app.get('/services/:id', async(req, res) => {
+		const id = req.params.id;
+		const query = { _id: new ObjectId(id) }
+
+		const options = {
+			// Include only the `title` and `imdb` fields in the returned document
+			projection: { title: 1, price: 1, service_id: 1 },
+		  };
+
+		const result = await serviceCollection.findOne(query, options);
+		res.send(result);
+	})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
